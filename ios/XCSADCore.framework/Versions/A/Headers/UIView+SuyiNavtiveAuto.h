31f17d27719b7c0d9bba207ad4fa5cc56a6692cb
maiya <maiya@ecook.cn>
2021-02-23 17:06:04 +0800

    misc:升级提交苏伊士自渲染信息流关闭按钮

---
+++ ios/XCSADCore.framework/Versions/A/Headers/UIView+SuyiNavtiveAuto.h
@@ -0,0 +1,20 @@
+//
+//  UIView+SuyiNavtiveAuto.h
+//  ADSuyiSDKDemo-iOS
+//
+//  Created by SJQ on 2021/2/23.
+//  Copyright © 2021 陈坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (SuyiNavtiveAuto)
+
+/// 信息流广告是否是自渲染广告
+@property (nonatomic, assign) BOOL isNeedUserShow;
+
+@end
+
+NS_ASSUME_NONNULL_END
